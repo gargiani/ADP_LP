d9223cf2cec9b210426014243a7d9bc361acc592
mgargiani <matildegargiani@gmail.com>
2020-11-06 18:51:26 +0100

    polishing

---
+++ files/bash.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python run_LTI_benchmark1.py -verbose 0 -save 1 -LP_approach 2 -n_constraints 50000 -std_dev 0.001 -samples 100 -exp_sampling 1 -u_range 1 -x_range 3 -symmetric 1 -random_seed 0
